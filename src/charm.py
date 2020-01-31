e32648fc36aa85ff16811218752573f38d62fce0
Josh Genet <joshua.genet@canonical.com>
2020-01-31 15:00:19 +0200

    fix incorrect symlink

--- src/charm.py
+++ src/charm.py
@@ -1,16 +1,29 @@
+#!/usr/bin/env python3
 import sys
-sys.path.append('lib')
+
+sys.path.append("lib")
 
 from ops.charm import CharmBase
 from ops.main import main
+from ops.framework import StoredState
+from ops.model import (
+    ActiveStatus,
+    BlockedStatus,
+    WaitingStatus,
+    MaintenanceStatus,
+)
+
 
 class MyCharm(CharmBase):
+    state = StoredState()
+
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.start, self.on_start)
+        self.framework.observe(self.on.install, self.on_install)
 
-     def on_start(self, event):
-        print("\n\n\n\nhello charm\n\n\n\n")
+    def on_install(self, event):
+        unit = self.framework.model.unit
+        unit.status = ActiveStatus()
 
 
 if __name__ == "__main__":
